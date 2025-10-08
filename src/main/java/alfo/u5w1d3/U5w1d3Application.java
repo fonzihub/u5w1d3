3725d94a6dd59f0b6fb772d1f77b4d3b89d98c7d
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-10-08 14:31:31 +0200

    Initial commit

---
+++ src/main/java/alfo/u5w1d3/U5w1d3Application.java
@@ -0,0 +1,13 @@
+package alfo.u5w1d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w1d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w1d3Application.class, args);
+	}
+
+}
---
+++ src/test/java/alfo/u5w1d3/U5w1d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package alfo.u5w1d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w1d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
